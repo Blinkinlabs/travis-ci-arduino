e6a2ce111a0a6eab78ad665a002529cefca036ee
Matt Mets <matt.mets@cibomahto.com>
2016-10-26 02:39:24 +0800

    Make board package name lowercase

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 # associative array for the platforms that will be verified in build_main_platforms()
 # this will be eval'd in the functions below because arrays can't be exported
 # export MAIN_PLATFORMS='declare -A main_platforms=( [uno]="arduino:avr:uno" [due]="arduino:sam:arduino_due_x" [zero]="arduino:samd:zero" [esp8266]="esp8266:esp8266:huzzah" [leonardo]="arduino:avr:leonardo" )'
-export MAIN_PLATFORMS='declare -A main_platforms=( [blinkytape]="Blinkinlabs:avr:blinkytape" [esp8266]="esp8266:esp8266:generic" )'
+export MAIN_PLATFORMS='declare -A main_platforms=( [blinkytape]="blinkinlabs:avr:blinkytape" [esp8266]="esp8266:esp8266:generic" )'
 
 # associative array for other platforms that can be called explicitly in .travis.yml configs
 # this will be eval'd in the functions below because arrays can't be exported
