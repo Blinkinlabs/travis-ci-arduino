f5b1044cda72d2d8dc0787a67d2c876199ed300a
Matt Mets <matt.mets@cibomahto.com>
2016-10-26 00:08:30 +0800

    Enable output so we can see what's breaking

--- install.sh
+++ install.sh
@@ -71,7 +71,8 @@ if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 # install random lib so the arduino IDE grabs a new library index
 # see: https://github.com/arduino/Arduino/issues/3535
 echo -n "UPDATE LIBRARY INDEX: "
-DEPENDENCY_OUTPUT=$(arduino --install-library USBHost > /dev/null 2>&1)
+#DEPENDENCY_OUTPUT=$(arduino --install-library USBHost > /dev/null 2>&1)
+DEPENDENCY_OUTPUT=$(arduino --install-library USBHost)
 if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 
 # set the maximal compiler warning level
