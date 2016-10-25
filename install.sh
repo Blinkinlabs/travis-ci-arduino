8879daa0a789d80f012b683ffaddf10eb68d010a
Matt Mets <matt.mets@cibomahto.com>
2016-10-26 00:20:37 +0800

    Try not including the blinkytape library

--- install.sh
+++ install.sh
@@ -64,9 +64,9 @@ if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 #DEPENDENCY_OUTPUT=$(arduino --install-boards adafruit:avr 2>&1)
 #if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 
-echo -n "BLINKINLABS AVR: "
-DEPENDENCY_OUTPUT=$(arduino --install-boards Blinkinlabs:avr 2>&1)
-if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
+#echo -n "BLINKINLABS AVR: "
+#DEPENDENCY_OUTPUT=$(arduino --install-boards Blinkinlabs:avr 2>&1)
+#if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 
 # install random lib so the arduino IDE grabs a new library index
 # see: https://github.com/arduino/Arduino/issues/3535
